b57eeb44ee823b53ec74ecb7fea2894dc2214f2b
Kirill <kirilltroyak10@gmail.com>
2022-03-31 13:21:12 +0300

    changed URL

--- main.py
+++ main.py
@@ -65,6 +65,7 @@ class ContactForm(FlaskForm):
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+db.create_all()
 
 
 ##CONFIGURE TABLES
