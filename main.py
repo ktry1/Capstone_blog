feb05bdbac6b076b5ddedf3b31e5dbcccef90d25
Kirill <kirilltroyak10@gmail.com>
2022-03-31 11:31:42 +0300

    started using PostgreSQL

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ class ContactForm(FlaskForm):
     submit=SubmitField("Send email")
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
